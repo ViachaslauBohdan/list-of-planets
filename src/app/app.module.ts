9b941fb540f06fc1a9b436feac166b3d65110625
Viachaslau Bohdan <viachaslau.bohdan@onemeter.com>
2020-06-07 18:54:53 +0200

    planet-details update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FormsModule } from '@angular/forms';
 import { PaginatorComponent } from './common/pagination/paginator.component';
 import {MatPaginatorModule} from '@angular/material/paginator';
 import { PlanetDetailsComponent } from './planets/planet-details/planet-details.component';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -33,7 +34,8 @@ import { PlanetDetailsComponent } from './planets/planet-details/planet-details.
     FormsModule,
     MatCardModule,
     HttpClientModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
